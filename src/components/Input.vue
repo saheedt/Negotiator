355e8de9e135620ffbc1018928f7a77fe5fe65b6
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-03-08 11:16:08 +0100

    add re-usable <Input /> component

---
+++ src/components/Input.vue
@@ -0,0 +1,30 @@
+<template>
+  <input
+    :aria-label="label"
+    :placeholder="placeholder"
+    :type="type"
+    :value="localValue"
+    v-on:input="$emit('input', $event.target.value)"
+    :required="required"
+  />
+</template>
+<script>
+export default {
+  name: 'Input',
+  props: {
+    label: String,
+    placeholder: String,
+    required: Boolean,
+    type: String,
+  },
+  data: () =>({
+      localValue: null
+  }),
+};
+</script>
+<style lang="scss">
+  input {
+    height: 5rem;
+  	width: 80%;
+	}
+</style>
