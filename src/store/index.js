2aa4cf113a8c6bcf8aa198424952047e163adbd6
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-03-11 15:33:16 +0100

    - add groupings unit tests - modify store to return new instance on store method call - add store mutation test

--- src/store/index.js
+++ src/store/index.js
@@ -6,21 +6,24 @@ import util from '../utils/util';
 Vue.use(Vuex);
 
 const { mutations } = util;
-export default new Vuex.Store({
-  state: {
-    minSalary: null,
-    maxOffer: null,
-  },
-  mutations: {
-    [mutations.SET_MIN_SALARY](state, value) {
-      state.minSalary = value;
+export default () => {
+  return new Vuex.Store({
+    state: {
+      minSalary: null,
+      maxOffer: null,
     },
-    [mutations.SET_MAX_OFFER](state, value) {
-      state.maxOffer = value;
+    mutations: {
+      [mutations.SET_MIN_SALARY](state, value) {
+        state.minSalary = value;
+      },
+      [mutations.SET_MAX_OFFER](state, value) {
+        state.maxOffer = value;
+      },
     },
-  },
-  actions: {
-  },
-  modules: {
-  },
-});
+    actions: {
+    },
+    modules: {
+    },
+  });
+};
+
